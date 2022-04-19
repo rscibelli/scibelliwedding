946d12b84f63064c34b2313333adfed82397203f
robertscibelli <robert.scibelli@libertymutual.com>
2022-04-18 22:48:47 -0400

    Add some stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* className="App-header" */}
+      <header></header>
+      <body>
+        <div className="main-photo">
+          <h2>Welcome to Jon Scibelli and Charlotte Lawsons's Wedding Website!</h2>
+        </div>
+        
+      </body>
     </div>
   );
 }
