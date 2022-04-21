4ec1dd195b255e64de3d71932e5ef71011e8d935
robertscibelli <robert.scibelli@libertymutual.com>
2022-04-20 22:09:21 -0400

    adding layers

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* className="App-header" */}
-      <header></header>
-      <body>
+      <body className="body">
         <div className="main-photo">
-          <h2>Welcome to Jon Scibelli and Charlotte Lawsons's Wedding Website!</h2>
+          <h2 className="title">Welcome to Jon Scibelli and Charlotte Lawsons's Wedding Website!</h2>
+        </div>
+        <div className="second-layer">
+          <p>Hello</p>
         </div>
         
       </body>
