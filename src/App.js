8bf2fafdc6c518f5633568d912790f4c15a5a865
robertscibelli <robert.scibelli@libertymutual.com>
2022-04-20 22:18:34 -0400

    spell check

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <body className="body">
         <div className="main-photo">
-          <h2 className="title">Welcome to Jon Scibelli and Charlotte Lawsons's Wedding Website!</h2>
+          <h2 className="title">Welcome to Jon Scibelli and Charlotte Lawson's Wedding Website!</h2>
         </div>
         <div className="second-layer">
           <p>Hello</p>
